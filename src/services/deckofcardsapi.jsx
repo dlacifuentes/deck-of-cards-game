ea618d5b2c607866729c3d7599e57a47d3b8b032
dlacifuentes <84488491+dlacifuentes@users.noreply.github.com>
2022-12-04 22:58:56 -0500

    Update deckofcardsapi.jsx

--- src/services/deckofcardsapi.jsx
+++ src/services/deckofcardsapi.jsx
@@ -1,12 +1,21 @@
+const cardsApi = 'https://deckofcardsapi.com/api/deck'
+
 const getIdGame = async () => {
-	const url = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
+	const url = `${cardsApi}/new/shuffle/?deck_count=1`;
 	const res = await fetch(url);
 	const data = await res.json();
 	return data?.deck_id;
 };
 
 const getCards = async deckId => {
-	const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=10`;
+	const url = `${cardsApi}/${deckId}/draw/?count=10`;
+	const res = await fetch(url);
+	const data = await res.json();
+	return data?.cards;
+};
+
+const getCard = async deckId => {
+	const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`;
 	const res = await fetch(url);
 	const data = await res.json();
 	return data?.cards;
@@ -15,6 +24,7 @@ const getCards = async deckId => {
 const DeckOfCardsAPI = {
 	getIdGame,
 	getCards,
+	getCard,
 };
 
 export default DeckOfCardsAPI;
